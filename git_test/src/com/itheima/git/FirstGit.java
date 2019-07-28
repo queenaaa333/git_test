2e9b5e56c55b2059c611c3774b6ac867d2525d37
zhao <zhao@itcast.com>
2019-07-28 14:15:44 +0800

    克隆下来的修改

--- git_test/src/com/itheima/git/FirstGit.java
+++ git_test/src/com/itheima/git/FirstGit.java
@@ -7,8 +7,12 @@ public class FirstGit {
         System.out.println("hello git1111111");
         System.out.println("hello git1111111");
         System.out.println("hello git1111111");
+        //克隆修改
+        System.out.println("clone update");
     }
     public void test01(){
         System.out.println("hello git 2222222222");
+        //克隆
+        System.out.println("3333333333333333333333333333333333333");
     }
 }
