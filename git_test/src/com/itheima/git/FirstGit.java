9a68ead82d2f5636c6a3c6f82850d2dc13b4e3e3
zhao <zhao@itcast.com>
2019-07-28 11:46:44 +0800

    git_test提交到本地仓库

---
+++ git_test/src/com/itheima/git/FirstGit.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git1111111");
+        System.out.println("hello git1111111");
+        System.out.println("hello git1111111");
+        System.out.println("hello git1111111");
+        System.out.println("hello git1111111");
+    }
+}
